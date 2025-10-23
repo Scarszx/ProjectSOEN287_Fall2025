29875dcc1961f3a54ec880373ad9b635bef4de26
Harsh Shewraj <harsh.shewraj@gmail.com>
2025-10-22 20:34:02 -0400

    login seems successful, gotta tidy up tho

--- js/create_account.js
+++ js/create_account.js
@@ -2,12 +2,20 @@ document.getElementById("registerForm").onsubmit = function(e){
     e.preventDefault();
 
     var username = document.getElementById("new_username").value;
-    var status = document.getElementById("status").value;
+    var statusElement = document.querySelector('input[name="status"]:checked');
+    var status = statusElement ? statusElement.value : '';
+
     var password = document.getElementById("new_password").value;
+    var confirmPassword = document.getElementById("confirm_password").value;
+    if(password !== confirmPassword){
+        alert("Passwords do not match!");
+        return;
+    }
+
 
     // Store as plain string in cookie: username|password|status
     document.cookie = "user=" + username + "|" + password + "|" + status + ";path=/";
 
     alert("Account created!");
-    window.location.href = "login_page.html";
+    window.location.href = "../index.html/login_page.html";
 };
