65bd614bfe98b55d25aa9cd25a7cda85572fd520
David L. <kali@kali.coba.net>
2025-10-28 21:39:27 -0400

    page 1 modif4

---
+++ js/page2.js
@@ -0,0 +1,14 @@
+//Script to load header dynamically -->
+
+fetch("header.html")
+    .then(res => res.text())
+    .then(html => {
+    document.getElementById("header-container").innerHTML = html;
+    })
+    .then(() => {
+    // Optional: dynamically set user info (simulate logged-in user)
+    const user = { name: "John Doe", role: "Student" };
+    document.getElementById("username").textContent = user.name;
+    document.getElementById("userRole").textContent = user.role;
+    })
+    .catch(err => console.error("Error loading header:", err));
